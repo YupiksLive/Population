8e5d623c0e832a01447b98bc78eff077b2a70018
YupiksLive <svs731g@gmail.com>
2022-04-12 19:18:49 +0300

    Подсчёт населения

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> name = Arrays.asList("Dima", "Sasha", "Nastya", "Artem", "Maxim");
+        List<String> lastName = Arrays.asList("Lunyakov", "Kholodkov", "Morzhakova", "Inozemtsev", "Spiridonov");
+        Collection<Person> people = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            people.add(new Person(name.get(new Random().nextInt(name.size())),
+                    lastName.get(new Random().nextInt(lastName.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)]));
+        }
+        System.out.println("Весь список:");
+        people.stream()
+                .forEach(System.out::println);
+        System.out.println("Совершеннолетние:");
+        people.stream()
+                .filter(person -> person.getAge() >= 18)
+                .forEach(System.out::println);
+        System.out.println("Призывники:");
+        people.stream()
+                .filter(person -> person.getAge() >= 18 && person.getAge() < 27)
+                .forEach(person -> System.out.println(person.getName() + " " + person.getFamily()));
+        System.out.println("Работоспособные люди:");
+        people.stream()
+                .filter(person -> (person.getSex().equals(Sex.WOMAN) && person.getAge() >= 18 && person.getAge() < 60) || (person.getSex().equals(Sex.MAN) && person.getAge() >= 18 && person.getAge() < 65))
+                .filter(person -> person.getEducation().equals(Education.HIGHER))
+                .forEach(person -> System.out.println(person.getName() + " " + person.getFamily()));
+    }
+}
